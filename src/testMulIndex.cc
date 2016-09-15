1d02b706b0d0f56300d36e46b3709121721bea9c
Ye Yu <sdyy1990@gmail.com>
2016-09-15 14:56:13 -0400

    fix bug: uint64_t index for large arrays

--- src/testMulIndex.cc
+++ src/testMulIndex.cc
@@ -4,11 +4,10 @@
 #include <cstdio>
 #include <unistd.h>
 #include "othello.h"
-#include "muloth.h"
+#include "mulothindex.h"
 #include <chrono>
 #include <inttypes.h>
 #include "io_helper.h"
-#include "time.h"
 using namespace std;
 typedef unsigned long long keyT;
 typedef uint64_t valueT;
@@ -21,7 +20,6 @@ IOHelper<keyT,valueT> *helper;
 
 
 int main(int argc, char * argv[]) {
-	srand(time(NULL));
     if (argc < 4) {
         printf(" splitbits keyInputFile OutputFile .... \n");
         printf(" splitbits: a number <=16, divide the keys into 2^(splitbits) sets, according to the Least (splitbit) significant bits. \n");
@@ -32,56 +30,18 @@ int main(int argc, char * argv[]) {
     sscanf(argv[1],"%d",&splitbit);
     helper = new ConstantLengthKmerHelper<keyT,valueT>(KMERLENGTH,splitbit);
 
-    MulOth<keyT> * moth;
+    MulOthIndex<keyT> * moth;
     if (splitbit >=0) {
         printf("Split %d groups\n",1U<< splitbit);
-        moth = new MulOth<keyT>(VALUELENGTH,argv[2],  splitbit, helper, true);
+        moth = new MulOthIndex<keyT>(argv[2],  splitbit, helper, true);
         if (!moth->buildsucc) return 1;
 
         printf("Build Succ, write to file %s\n", argv[3]);
         //moth.printall();
         moth->writeToFile(argv[3]);
-
-
-
-
-
-
-
-        printf("Testing using keys from file %s\n", argv[2]);
-        FILE *pFile;
-        pFile = fopen (argv[2],"r");
-        uint64_t cnt = 0;
-        while (true) {
-            char buf[1024];
-            if (fgets(buf,1024,pFile)==NULL) break;
-            keyT k;
-            valueT v;
-            if (!helper->convert(buf, &k, &v)) break;
-            valueT qv = moth->query(k);
-            if ((qv & ((1<<VALUELENGTH)-1))!=(v & ((1<<VALUELENGTH)-1) )) {
-                printf("Err %llx -> %x : %x\n", k,
-                        v & ((1<<VALUELENGTH)-1),
-                        qv & ((1<<VALUELENGTH)-1));
-                fclose(pFile);
-                return 0;
-            }
-            //    printf("%" PRIx64"--> %d\n", k,v);
-	}
-	fclose(pFile);
-
-
-
-
-
-
-
-
-
-
         delete moth;
     }
-    moth = new MulOth<keyT>( argv[3],helper);
+    moth = new MulOthIndex<keyT>( argv[3],helper);
 
     for (int i = 2; i< argc; i+=2) {
         printf("Testing using keys from file %s\n", argv[i]);
@@ -95,14 +55,6 @@ int main(int argc, char * argv[]) {
             valueT v;
             if (!helper->convert(buf, &k, &v)) break;
             valueT qv = moth->query(k);
-            if ((qv & ((1<<VALUELENGTH)-1))!=(v & ((1<<VALUELENGTH)-1) )) {
-                printf("Err %llx -> %x : %x\n", k,
-                        v & ((1<<VALUELENGTH)-1),
-                        qv & ((1<<VALUELENGTH)-1));
-                fclose(pFile);
-                return 0;
-            }
-            //    printf("%" PRIx64"--> %d\n", k,v);
 
         }
         fclose(pFile);
