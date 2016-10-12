b3d0743e6e5273d4708a427ee7aeca6ed2646fc9
Ye Yu <sdyy1990@gmail.com>
2016-10-11 21:28:16 -0400

    sorter

--- src/SortAndCompress.cc
+++ src/SortAndCompress.cc
@@ -6,20 +6,18 @@ using namespace std;
 int main(int argc, char * argv[]) {
     ConstantLengthKmerHelper<uint64_t, uint16_t> iohelper(20,0);
     KmerFileReader< uint64_t,uint16_t > freader(argv[1], &iohelper);
-    BinaryKmerWriter< uint64_t > fwriter(argv[2]);
     KVpair<uint64_t, uint16_t> pp;
     vector<uint64_t> VKmer;
     while (freader.getNext(&pp.k, &pp.v)) {
-        fwriter.write(&pp.k);
         VKmer.push_back(pp.k);
     }
-    fwriter.finish();
     printf("Sorting\n");
     sort(VKmer.begin(),VKmer.end());
-    BinaryKmerReader< uint64_t > breader(argv[2]);
-    while (breader.getNext(&pp.k)) {
+    BinaryKmerWriter< uint64_t > fwriter(argv[2]);
+    for (auto a: VKmer) {
+        fwriter.write(&a);
     }
-
+    fwriter.finish();
    return 0; 
 
 }
