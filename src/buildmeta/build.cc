6ff73cc29378e2457ef0f823d21617bb3354ad9e
Ye Yu <sdyy1990@gmail.com>
2016-10-16 21:02:11 -0400

    meta build

---
+++ src/buildmeta/build.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include "io_helper.h"
+#include "muloth.h"
+#include <chrono>
+#include <inttypes.h>
+#include <string>
+using namespace std;
+typedef uint64_t keyT;
+typedef uint16_t valueT;
+
+
+int main(int argc, char * argv[]) { 
+    if (argc < 5) {
+        printf("args: descriptiveFilename KmerFnamePrefix KmerFnameSuffix Kmer_length splitbits OutputFile\n");
+        return 0;
+    }    
+    string Kmer_length_str = argv[4];
+    string splitbitStr = argv[5];
+    int Kmer_length = atoi(Kmer_length_str.c_str());
+    int splitbit = atoi(splitbitStr.c_str());
+    
+    IOHelper<keyT,uint16_t> *helper;
+    helper = new ConstantLengthKmerHelper<keyT,uint16_t>(Kmer_length,splitbit);
+    taxoTreeBuilder<uint64_t, uint16_t> builder(argv[1],argv[2],argv[3],helper);
+
+    MulOth<keyT,uint16_t> * moth;
+    moth = new MulOth<keyT,uint16_t>(VALUELENGTH,splitbit, &builder);
+
+    if (!moth->buildsucc) return 1;
+    printf("Build Succ, write to file %s\n", argv[6]);
+    moth->writeToFile(argv[6]);
+    delete moth;
+    
+}
+
+
+
+
+
+
