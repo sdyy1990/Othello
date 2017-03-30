e62bff1485fd96fdce33b049a6c7a0208c3e56c4
sdyy1990 <sdyy1990@gmail.com>
2017-03-30 15:40:01 -0400

    setAlienPref

--- src/testUpdate.cc
+++ src/testUpdate.cc
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include "io_helper.h"
 #include "time.h"
+#include <cmath>
 using namespace std;
 typedef unsigned long long keyT;
 typedef uint16_t valueT;
@@ -28,6 +29,28 @@ bool inline check(Othello<keyT> & oth , keyT key, valueT value) {
     }
     return true;
 
+}
+void printHisto(map<valueT, int> &vv) {
+    double s = 0.0, s2 = 0.0;
+    for (auto &a:vv) {
+        s += a.second;
+        s2 += ((double) a.second)*(a.second);
+        //printf("%d:%d\t", a.first, a.second);
+    }
+    double avg = s*1.0/vv.size();
+    printf("avg = %.1lf, stdE = %.1lf, cnt = %d\n", avg, sqrt(s2/vv.size() - avg*avg), vv.size()); 
+
+}
+void computeRates(Othello<keyT> &oth) {
+    map<valueT,int> vv;
+    int NNN = 104857600;
+    for (int i = 0; i < NNN; i++) {
+        keyT key = (((unsigned long long) rand())<<32) + rand();
+        valueT ans =  oth.queryInt(key);
+//        valueT ans = rand();
+        vv[ans] ++;
+    }
+    printHisto(vv);
 }
 int main(int argc, char * argv[]) {
     keys = (keyT*) valloc(sizeof(keyT) * 4096);
@@ -72,7 +95,22 @@ int main(int argc, char * argv[]) {
         oth.updatevalue(i,&(values[0]),sizeof(values[0]));
         if (!check(oth, keys[i], values[i])) printf("Err at %d\n",i); 
     }
+
+    oth.setAlienPreference(&(values[0]), sizeof(values[0]), 0.5);
+    printf("Othello size = %d\n", oth.mykeycount);    for (int i = 0 ; i < oth.mykeycount; i++) if (!check(oth, keys[i], values[i])) printf("Err at %d\n",i); 
+    computeRates(oth);
+
+    oth.setAlienPreference(&(values[0]), sizeof(values[0]), 0.8);
+    printf("Othello size = %d\n", oth.mykeycount);    for (int i = 0 ; i < oth.mykeycount; i++) if (!check(oth, keys[i], values[i])) printf("Err at %d\n",i); 
+    computeRates(oth);
+    for (double pref = 0.1; pref < 0.5; pref+=0.049) {
+    oth.setAlienPreference(&(values[0]), sizeof(values[0]), pref);
+    printf("Othello size = %d\n", oth.mykeycount);    for (int i = 0 ; i < oth.mykeycount; i++) if (!check(oth, keys[i], values[i])) printf("Err at %d\n",i); 
+    computeRates(oth);
+    }
+
     free(keys);
     free(values);
     return 0;
+    
 }
